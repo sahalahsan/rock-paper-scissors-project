374ab18355b8ad64dde5c262b983bc478c3d1bc6
sahalahsan <sahaledu000@gmail.com>
2023-01-01 07:05:55 -0500

    The code takes players choice and then runs getComputersChoice function second before comparing them with playRound function giving only accurate value when both choices are same.To make the playersSelection case insensitive it is converted to titleCase with string methods.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,31 @@
-//this asks for players choice
-const playerSelection = prompt("Select your option:");
+//this asks for the player choice
+let playerChoice = prompt("Select your option:");
+//making the playerChoice titlecase(Only first letter is capital)
+let fisrtLetterSmall = playerChoice.charAt(0);
+let fisrtLetterCapital = fisrtLetterSmall.toUpperCase();
+let remainingLetters = playerChoice.slice(1);
+let remainingLetterssmall = remainingLetters.toLowerCase();
+const playerSelection = fisrtLetterCapital + remainingLetterssmall
 console.log(playerSelection);
 
-//function which selects randomly from Rock,Paper and Scissors
-//Array from which getComputerChoice function select option
+//function which selects randomly from Rock,Paper and Scissors as computer choice
 const computerChoice = ["Rock", "Paper", "Scissors"];
 function getComputerChoice(min, max){
     let num = Math.floor(Math.random() * (max - min + 1) + min );
     return computerChoice[num] ;
     }
         
-const computerSelection = getComputerChoice(0,2);
+let computerSelection = getComputerChoice(0,2);
 console.log(computerSelection);
-
+ 
+//function which copmare both options and returns result
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+         return "A Tie";
+    } else {
+         return "Result";
+    }
+}
+const result =  playRound(playerSelection, computerSelection);
+console.log(result);
 
