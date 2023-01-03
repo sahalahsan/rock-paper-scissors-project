917f7c681e97c8bb322455ba5da102308119f311
sahalahsan <sahaledu000@gmail.com>
2023-01-03 00:13:23 -0500

    Wrote the function playRound which plays a single round of Rock,Paper Scissors and return the string as aresult.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,8 @@
 //this asks for the player choice
 let playerChoice = prompt("Select your option:");
 //making the playerChoice titlecase(Only first letter is capital)
-let fisrtLetterSmall = playerChoice.charAt(0);
-let fisrtLetterCapital = fisrtLetterSmall.toUpperCase();
-let remainingLetters = playerChoice.slice(1);
-let remainingLetterssmall = remainingLetters.toLowerCase();
+let fisrtLetterCapital = playerChoice.charAt(0).toUpperCase();
+let remainingLetterssmall =  playerChoice.slice(1).toLowerCase();
 const playerSelection = fisrtLetterCapital + remainingLetterssmall
 console.log(playerSelection);
 
@@ -18,14 +16,28 @@ function getComputerChoice(min, max){
 let computerSelection = getComputerChoice(0,2);
 console.log(computerSelection);
  
-//function which copmare both options and returns result
+//function which copmares both options and return results
 function playRound(playerSelection, computerSelection){
     if(playerSelection == computerSelection){
          return "A Tie";
-    } else {
-         return "Result";
-    }
+    } else if(
+      playerSelection == "Rock" && computerSelection == "Scissors"  ||    
+      playerSelection == "Paper" && computerSelection == "Rock"  ||
+      playerSelection == "Scissors" && computerSelection == "Paper"){
+         return `You Won!${playerSelection} beats ${computerSelection}`;
+    } else{
+        return  `You Lose!${computerSelection} beats ${playerSelection}`;
+    }   
 }
 const result =  playRound(playerSelection, computerSelection);
 console.log(result);
 
+//function which loops through playRound five times
+//function game(){
+    //for(var i=1; i <= 5; i++){
+       //let n = playRound(playerSelection, computerSelection);
+      // console.log(playRound(playerSelection, computerSelection));
+    //}
+//}
+//console.log(game());
+//console.log(playRound(playerSelection, computerSelection));
