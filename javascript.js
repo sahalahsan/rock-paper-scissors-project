d0ed09f2ec90ac1586967d416408c0ff7e4c6a6d
sahalahsan <sahaledu000@gmail.com>
2023-01-25 12:55:41 -0500

    On-click functions added to buttons which console.log result

--- javascript.js
+++ javascript.js
@@ -1,53 +1,62 @@
-let playerSelection;
-let computerSelection;
-let tied = 0;
-let won = 0;
-let lost = 0;
-
-//Function that plays single round of Rock Paper Scissors
-function playRound(playerSelection, computerSelection){
-
-  //User Input  
-  const playerChoice = prompt("Select your option:");
-  let fisrtLetterCapital = playerChoice.charAt(0).toUpperCase();
-  let remainingLetterssmall =  playerChoice.slice(1).toLowerCase();
-  playerSelection = fisrtLetterCapital + remainingLetterssmall;
-
-  const computerChoice = ["Rock", "Paper", "Scissors"];
-
-  //Function that randomly selects Rock Paper Scissors as Computers Choice
-  function getComputerChoice(min, max){
-    let num = Math.floor(Math.random() * (max - min + 1) + min );
-    return computerChoice[num] ;
+function functionOne(playerSelection = "Rock", computerSelection){
+  function randomOption(){
+    const optionsArray = ["Rock", "Paper", "Scissors"];
+    const randomIndexNo =  Math.floor(Math.random()*optionsArray.length);
+    return optionsArray[randomIndexNo];
   }
-  computerSelection = getComputerChoice(0,2);
+  computerSelection = randomOption();
+
   if(playerSelection == computerSelection){
-      ++tied;
-      return "A Tie";
-    } else if(
-      playerSelection == "Rock" && computerSelection == "Scissors"  ||    
-      playerSelection == "Paper" && computerSelection == "Rock"  ||
-      playerSelection == "Scissors" && computerSelection == "Paper"){
-      ++won;
-      return `You Won!${playerSelection} beats ${computerSelection}`;
-    } else{
-      ++lost;
-      return  `You Lose!${computerSelection} beats ${playerSelection}`;
-    }   
+    console.log("A Tie");
+  } else if(
+    playerSelection == "Rock" && computerSelection == "Scissors"  ||    
+    playerSelection == "Paper" && computerSelection == "Rock"  ||
+    playerSelection == "Scissors" && computerSelection == "Paper"){
+    console.log(`You Won!${playerSelection} beats ${computerSelection}`);
+  } else{
+    console.log( `You Lose!${computerSelection} beats ${playerSelection}`);
+  }   
 }
 
-//Function which runs playRound function five times and announce rounds result
-function game(){
-  for(let i=0;i<5;i++){
-    console.log(playRound(playerSelection, computerSelection));
+//Function Two
+function functionTwo(playerSelection = "Paper", computerSelection){
+  function randomOption(){
+    const optionsArray = ["Rock", "Paper", "Scissors"];
+    const randomIndexNo =  Math.floor(Math.random()*optionsArray.length);
+    return optionsArray[randomIndexNo];
   }
-  if(tied === 5 || won === lost){
-    return "Round Tied";
-  } else if(won > lost){
-    return "You Won the Round";
+  computerSelection = randomOption();
+
+  if(playerSelection == computerSelection){
+    console.log("A Tie");
+  } else if(
+    playerSelection == "Rock" && computerSelection == "Scissors"  ||    
+    playerSelection == "Paper" && computerSelection == "Rock"  ||
+    playerSelection == "Scissors" && computerSelection == "Paper"){
+    console.log(`You Won!${playerSelection} beats ${computerSelection}`);
   } else{
-    return "You Lost the Round";
-  }
+    console.log( `You Lose!${computerSelection} beats ${playerSelection}`);
+  }   
 }
-console.log(game());
- 
+
+//Function Three
+function functionThree(playerSelection = "Scissors", computerSelection){
+  function randomOption(){
+    const optionsArray = ["Rock", "Paper", "Scissors"];
+    const randomIndexNo =  Math.floor(Math.random()*optionsArray.length);
+    return optionsArray[randomIndexNo];
+  }
+  computerSelection = randomOption();
+
+  if(playerSelection == computerSelection){
+    console.log("A Tie");
+  } else if(
+    playerSelection == "Rock" && computerSelection == "Scissors"  ||    
+    playerSelection == "Paper" && computerSelection == "Rock"  ||
+    playerSelection == "Scissors" && computerSelection == "Paper"){
+    console.log(`You Won!${playerSelection} beats ${computerSelection}`);
+  } else{
+    console.log( `You Lose!${computerSelection} beats ${playerSelection}`);
+  }   
+}
+
